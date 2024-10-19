b27be290b1112715143ad40fdbd132437f4b04a4
KanchanBora <kanchanbora31@gmail.com>
2024-10-19 21:36:28 +0530

    add res

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region     = "us-west-2"
 }
+resource "aws_instance" "Demo1" {
+  ami           = "ami-07c5ecd8498c59db5"
+  instance_type = "t2.micro"
+  key_name = "kanch"
+  tags = {
+    Name = "Terraform Demo"
+  }
+}
